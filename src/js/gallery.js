aeb74e37481462ca1099d707ede66c15985ffe54
YelyzavetaMala <malaelizaveta678@gmail.com>
2025-02-25 16:04:16 +0200

    fixed swiper

---
+++ src/js/gallery.js
@@ -0,0 +1,39 @@
+import Swiper from 'swiper';
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  Keyboard,
+  Mousewheel,
+} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiperParams = {
+  modules: [Pagination, Navigation, Scrollbar, Keyboard, Mousewheel],
+
+  breakpoints: {
+    375: { slidesPerView: 1, spaceBetween: 6 },
+    834: { slidesPerView: 2, spaceBetween: 16 },
+    1440: { slidesPerView: 3.5, spaceBetween: 16 },
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  keyboard: {
+    enabled: true,
+  },
+
+  mousewheel: true,
+};
+
+const swiper = new Swiper('.swiper-gallery-init', swiperParams);
---
+++ src/js/tours.js
@@ -0,0 +1,43 @@
+import Swiper from 'swiper';
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  Keyboard,
+  Mousewheel,
+} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiperParams = {
+  modules: [Pagination, Navigation, Scrollbar, Keyboard, Mousewheel],
+
+  breakpoints: {
+    375: { slidesPerView: 1, spaceBetween: 32 },
+    834: { slidesPerView: 1, spaceBetween: 32 },
+
+    1440: {
+      slidesPerView: 2,
+      spaceBetween: 32,
+    },
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  keyboard: {
+    enabled: true,
+  },
+
+  mousewheel: true,
+};
+
+const swiper = new Swiper('.swiper-tours-init', swiperParams);
